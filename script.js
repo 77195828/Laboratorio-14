508937dd2068374300a10deaa0ea9b138c9a4091
77195828 <alejandrogomezsolano@hotmail.com>
2024-11-04 18:35:01 -0500

    Intro 1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded',function(){
    const button = document.getElementById('changeTextButton');
-   button.addEventListener=('click', function(){
-    const IntroSection= document.getElementById('intro');
+   button.addEventListener('click', function(){
+    const IntroSection=document.getElementById('intro');
   IntroSection.innerHTML= '<p>El texto ha cambiado despues de hacer click en el boton! </p>';
   });
  });
